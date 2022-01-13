30bfc56c60951fe2867bcff77b7ccf68ab90440c
Eric <eric.robert@simpleclinic.net>
2022-01-13 13:13:59 +1000

    try to avoid contaminate global ns

--- src/Db.php
+++ src/Db.php
@@ -5,8 +5,9 @@ namespace Codeidb;
 class Db{
     protected $db;
 
-    public function __construct($config)
+    public function __construct(array $config = [])
     {
-
+        $DB = (require_once("./lib/DB.php"));
+        $this->db = $DB($config);
     }
 }
